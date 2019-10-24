15fc3a3d140575d2e1608c8a9fd1972d1df2bc79
PratikP <pratikp>
2019-10-24 16:19:46 +0530

    Initial Commit

---
+++ BarCode_QRScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarCode_QRScanner
+//
+//  Created by Patrick on 24/10/19.
+//  Copyright © 2019 Patrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
